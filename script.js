e35097b12e430aa4efc8784b4532e0d42d079c34
Lexandra-Ore <ore.lex@yahoo.com>
2021-06-06 15:05:47 -0700

    social links

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 document.querySelector(".bluePhoneThumb").onclick = function () {
     document.querySelector(".iphone").src = "blue.png";
-    document.getElementById("linkColor").style.background = "#14366b";
+    document.getElementById("linkColor").style.background = "#173b7d";
+    const socialColors = document.querySelectorAll(".socialColor");
+    socialColors.forEach(function(socialColor) {
+        socialColor.style.color = "#173b7d";
+    });
     const textColors = document.querySelectorAll(".textColor");
     textColors.forEach(function(textColor) {
-        textColor.style.color = "#14366b";
+        textColor.style.color = "#173b7d";
     });
     
 }
@@ -11,6 +15,10 @@ document.querySelector(".bluePhoneThumb").onclick = function () {
 document.querySelector(".blackPhoneThumb").onclick = function () {
     document.querySelector(".iphone").src = "black.png";
     document.getElementById("linkColor").style.background = "#4e5580";
+    const socialColors = document.querySelectorAll(".socialColor");
+    socialColors.forEach(function(socialColor) {
+        socialColor.style.color = "#4e5580";
+    });
     const textColors = document.querySelectorAll(".textColor");
     textColors.forEach(function(textColor) {
         textColor.style.color = "#4e5580";
@@ -20,6 +28,10 @@ document.querySelector(".blackPhoneThumb").onclick = function () {
 document.querySelector(".whitePhoneThumb").onclick = function () {
     document.querySelector(".iphone").src = "white.png";
     document.getElementById("linkColor").style.background = "#972000";
+    const socialColors = document.querySelectorAll(".socialColor");
+    socialColors.forEach(function(socialColor) {
+        socialColor.style.color = "#972000";
+    });
     const textColors = document.querySelectorAll(".textColor");
     textColors.forEach(function(textColor) {
         textColor.style.color = "#972000";
